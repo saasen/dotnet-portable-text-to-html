942cc6b2093cd3dc62a5b8ae6aa49e21f1594da7
Anders Stensaas <saasen@Anderss-MacBook-Pro.local>
2021-05-16 14:23:42 +0200

    change framework to net5.0 and add first test

--- SanityClient/Class1.cs
+++ SanityClient/Class1.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Net.Http;
+using System.Text.Json;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SanityClient
 {
     interface ISanityCdnClient
     {
-        Task<T> Query<T>(string query);
+        Task<T> Query<T>(string query, CancellationToken cancellationToken);
         //HttpResponseMessage Query(string query);
         //Task<T> GetDocument<T>(string query);
     }
@@ -17,46 +19,57 @@ namespace SanityClient
 
     public class SanityClient : ISanityClient
     {
-        private readonly HttpClient httpClient;
+        private readonly HttpClient _httpClient;
 
         // To accept a httpclient or not to, or just httphandler
+        // its nice to let the user pass httpclient so they can control how it is instantiated
         public SanityClient(HttpClient httpClient, SanityClientOptions options = null)
         {
             Guard.AgainstNull(options, nameof(options));
             Guard.AgainstNull(options.ProjectId, nameof(options.ProjectId));
+            Guard.AgainstNull(options.Dataset, nameof(options.Dataset));
 
-            // Or store directly in httpclient?
-            this.httpClient = httpClient;
-            this.httpClient.BaseAddress = new Uri($"https://{options.ProjectId}.api.sanity.io/v2021-03-25/");
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri($"https://{options.ProjectId}.api.sanity.io/v2021-03-25/");
         }
 
-        public Task<T> Query<T>(string query)
+        public async Task<T> Query<T>(string query, CancellationToken cancellationToken = default(CancellationToken))
         {
+            // TODO: HTTP encode
+            var response = await _httpClient.GetAsync($"data/query/production?query={query}", HttpCompletionOption.ResponseHeadersRead, cancellationToken);
+            response.EnsureSuccessStatusCode();
 
+            var typedContent = await JsonSerializer.DeserializeAsync<T>(await response.Content.ReadAsStreamAsync(cancellationToken), new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+            }, cancellationToken: cancellationToken);
+            return typedContent;
         }
     }
 
     public class SanityClientOptions
     {
+
         public string ProjectId { get; set; }
+        public string Dataset { get; set; }
         //public string Token { get; set; }
     }
 
     static class Guard
     {
-        public static void AgainstNull(string value, string paramName)
+        public static void AgainstNull(string value, string paramName, string message = null)
         {
             if (string.IsNullOrWhiteSpace(value))
             {
-                throw new ArgumentNullException(paramName);
+                throw new ArgumentNullException(paramName, message);
             }
         }
 
-        public static void AgainstNull<T>(T value, string paramName)
+        public static void AgainstNull<T>(T value, string paramName, string message = null)
         {
             if (value == null)
             {
-                throw new ArgumentNullException(paramName);
+                throw new ArgumentNullException(paramName, message);
             }
         }
     }
--- test/Tests/SanityClientTests.cs
+++ test/Tests/SanityClientTests.cs
@@ -1,17 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using FluentAssertions;
+using RichardSzalay.MockHttp;
 using SanityClient;
+using Snapshooter.Xunit;
+using Xunit;
 
 namespace Tests
 {
     public class SanityClientTests
     {
-        public void ShouldThrowOnNullHttpClient()
+        [Fact]
+        public async Task ShouldThrowOnNullHttpClient()
         {
-            var client = new SanityClient.SanityClient();
+            var mockHttp = new MockHttpMessageHandler();
+            mockHttp
+                .When("https://123456.api.sanity.io/v2021-03-25/data/query/production?query=*[_type == 'article']")
+                .Respond("application/json", @"[{""_id"": ""1234"", ""_type"" : ""article"", ""title"": ""title1""}]");
 
+            var client = new SanityClient.SanityClient(mockHttp.ToHttpClient(), new SanityClientOptions
+            {
+                ProjectId = "123456",
+                Dataset = "production"
+            });
+
+            var result = await client.Query<Article[]>("*[_type == 'article']");
+            result.Should().HaveCount(1);
+            result[0].Id.Should().Be("1234");
+            result[0].Type.Should().Be("article");
+            result[0].Title.Should().Be("title1");
         }
 
         //public async Task SimpleQuery()
@@ -46,9 +66,17 @@ namespace Tests
         //}
     }
 
-    class Article
+    class SanityDocument
+    {
+        [JsonPropertyName("_id")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("_type")]
+        public string Type { get; set; }
+    }
+
+    class Article : SanityDocument
     {
         public string Title { get; set; }
-        public dynamic Description { get; set; }
     }
 }
